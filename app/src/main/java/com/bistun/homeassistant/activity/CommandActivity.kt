9400aafbef726b8a69cc4c12e0fcd415a0988fa2
Ali zareshahi <alititi96@gmail.com>
2021-12-07 14:55:43 +0330

    send color and rang

--- app/src/main/java/com/bistun/homeassistant/activity/CommandActivity.kt
+++ app/src/main/java/com/bistun/homeassistant/activity/CommandActivity.kt
@@ -1,35 +1,101 @@
 package com.bistun.homeassistant.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.bistun.homeassistant.R
 import com.bistun.homeassistant.`interface`.DeviceService
-import com.bistun.homeassistant.adapter.DevicesAdapter
 import com.bistun.homeassistant.model.DeviceCommand
-import com.bistun.homeassistant.model.DeviceModel
 import com.bistun.homeassistant.util.RetrofitInstance
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
+import com.xw.repo.BubbleSeekBar
+import org.json.JSONArray
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import java.lang.Exception
+
 
 class CommandActivity : AppCompatActivity(),View.OnClickListener {
     private var deviceId:Int=0
     private var commandList:List<DeviceCommand> = emptyList()
     private lateinit var toggleBtn:Button
+    private lateinit var rangBar: BubbleSeekBar
+    private lateinit var colorBtn:Button
     private var countId:Int=10
+    private var rangBrigth:Int=0
+    private var redColor:Int = 0
+    private var greenColor:Int = 0
+    private var blueColor:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_command)
         deviceId =Integer.parseInt(intent.getStringExtra("device_id").toString())
-        Log.e("rec:",intent.getStringExtra("device_id").toString())
         toggleBtn =findViewById(R.id.toggle_btn)
+        rangBar =findViewById(R.id.rang_btn)
+        colorBtn =findViewById(R.id.color_btn)
+        rangBar.onProgressChangedListener=object:BubbleSeekBar.OnProgressChangedListener{
+            override fun onProgressChanged(
+                bubbleSeekBar: BubbleSeekBar?,
+                progress: Int,
+                progressFloat: Float,
+                fromUser: Boolean
+            ) {
+                Log.e("progress1:", progress.toString())
+            }
+
+            override fun getProgressOnActionUp(
+                bubbleSeekBar: BubbleSeekBar?,
+                progress: Int,
+                progressFloat: Float
+            ) {
+                Log.e("progress2:", progress.toString())
+            }
+
+            override fun getProgressOnFinally(
+                bubbleSeekBar: BubbleSeekBar?,
+                progress: Int,
+                progressFloat: Float,
+                fromUser: Boolean
+            ) {
+                rangBrigth =progress
+                sendColor()
+            }
+
+        }
         toggleBtn.setOnClickListener(this)
+        colorBtn.setOnClickListener(this)
         getDeviceInfo(deviceId)
     }
+
+    private fun sendColor() {
+        for(command in commandList){
+            if(command.inputType.equals("color")){
+                try {
+                    var json =JSONObject(command.commandJson)
+                    var service =JSONObject(json.getString("service_data"))
+                    service.put("rgb_color",JSONArray(intArrayOf(redColor,greenColor,blueColor)))
+                    service.put("brightness",rangBrigth)
+                    json.put("service_data",service)
+                    json.put("id",countId++)
+                    Log.e("sned:",json.toString())
+                    MainActivity.ws.sendText(json.toString())
+
+                }catch (e:Exception){
+                    Log.e("exception:",e.message?:"")
+                }
+
+            }
+        }
+    }
+
+
 
     private fun getDeviceInfo(deviceId: Int) {
         val retrofit: Retrofit? = RetrofitInstance.getRetrofitInstance()
@@ -39,14 +105,14 @@ class CommandActivity : AppCompatActivity(),View.OnClickListener {
                 call: Call<List<DeviceCommand>>,
                 response: Response<List<DeviceCommand>>
             ) {
-                if (response.body()?.size!!>0){
+                if (response.body()?.size!! > 0) {
                     commandList = response.body()!!
                 }
 
             }
 
             override fun onFailure(call: Call<List<DeviceCommand>>, t: Throwable) {
-                Log.e("onFailure:",t.message!!)
+                Log.e("onFailure:", t.message!!)
             }
 
 
@@ -55,13 +121,49 @@ class CommandActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onClick(p0: View?) {
         when(p0){
-            toggleBtn->sendCommandToggle()
+            toggleBtn -> sendCommandToggle()
+            colorBtn -> openColorPicker()
         }
     }
 
+    private fun openColorPicker() {
+        ColorPickerDialog.Builder(this)
+            .setTitle("ColorPicker Dialog")
+            .setPreferenceName("MyColorPickerDialog")
+            .setPositiveButton(getString(R.string.confirm),object :ColorEnvelopeListener{
+                override fun onColorSelected(envelope: ColorEnvelope?, fromUser: Boolean) {
+                    redColor =envelope?.argb?.get(1)!!
+                    greenColor=envelope?.argb?.get(2)!!
+                    blueColor =envelope?.argb?.get(3)!!
+                    sendColor()
+                }
+
+            })
+            .setNegativeButton(
+                getString(R.string.cancel)
+            ) { dialogInterface, i -> dialogInterface.dismiss() }
+            .attachAlphaSlideBar(true) // the default value is true.
+            .attachBrightnessSlideBar(true) // the default value is true.
+            .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
+            .show()
+    }
+
     private fun sendCommandToggle() {
-        val msg ="{\"type\":\"call_service\",\"domain\":\"homeassistant\",\"service\":\"toggle\",\"service_data\":{\"entity_id\":\"light.172_16_27_110\"},\"id\":${countId++}}"
-        Log.e("send:",msg)
-        MainActivity.ws.sendText(msg)
+        for(command in commandList){
+            if(command.inputType.equals("toggle")){
+                try {
+                    val json =JSONObject(command.commandJson)
+                    json.put("id",countId++)
+                    Log.e("send:", json.toString())
+                    MainActivity.ws.sendText(json.toString())
+                }catch (e:Exception){
+                    Log.e("ex",e.message?:"")
+                }
+
+            }
+        }
+
     }
 }
+
+
--- app/src/main/java/com/bistun/homeassistant/config/Config.kt
+++ app/src/main/java/com/bistun/homeassistant/config/Config.kt
@@ -1,5 +1,5 @@
 package com.bistun.homeassistant.config
 
 internal object  Config {
-    val DIR_API_URL:String ="http://172.17.10.229:4000/api/"
+    val DIR_API_URL:String ="http://172.17.10.252:4000/api/"
 }
